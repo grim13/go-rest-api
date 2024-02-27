b03c140196354a8518022beb2f333da730830aa3
Muhammad Bunyamin <muhammad.bunyamin@radenfatah.ac.id>
2024-02-27 11:21:35 +0700

    setup gin

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/events", getEvents) //GET, POST, PUT, PATCH, DELETE
+
+	server.Run(":8080") //localhost:8080
+}
+
+func getEvents(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{
+		"messege": "Hello!",
+	})
 }
