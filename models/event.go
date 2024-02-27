30fce0dbfabe3d2e2b1529d99589e74023dd5815
Muhammad Bunyamin <muhammad.bunyamin@radenfatah.ac.id>
2024-02-27 12:03:49 +0700

    add create and get event route

--- models/event.go
+++ models/event.go
@@ -3,19 +3,21 @@ package models
 import "time"
 
 type Event struct {
-	ID          int64
-	Name        string
-	Description string
-	Location    string
-	DateTime    time.Time
-	UserID      int
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
+	ID          int64     `json:"id"`
+	Name        string    `binding:"required"json:"name"`
+	Description string    `binding:"required"json:"description"`
+	Location    string    `binding:"required"json:"location"`
+	DateTime    time.Time `binding:"required"json:"dateTime"`
+	UserID      int       `json:"userId"`
+	CreatedAt   time.Time `json:"createdAt"`
+	UpdatedAt   time.Time `json:"updatedAt"`
 }
 
 var events = []Event{}
 
 func (e Event) Save() {
+	e.CreatedAt = time.Now()
+	e.UpdatedAt = time.Now()
 	events = append(events, e)
 }
 
