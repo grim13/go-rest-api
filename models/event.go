80ce910be63182368846866f243b38ffafb3d0bb
muhammad bunyamin <bunyamin250391@gmail.com>
2024-02-27 23:37:45 +0700

    add update event feature

--- models/event.go
+++ models/event.go
@@ -70,3 +70,21 @@ func GetEventByID(id int64) (*Event, error) {
 	}
 	return &event, nil
 }
+
+func (e *Event) Update() error {
+	e.UpdatedAt = time.Now()
+
+	query := `
+		UPDATE events
+		SET 
+			name = ?, description = ?, location = ?, dateTime = ?, updated_at = ?
+		WHERE
+			id = ?`
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UpdatedAt, e.ID)
+	return err
+}
