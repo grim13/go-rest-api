1fc4126af4de7a8bddd3834aaac8b9c3808dec94
muhammad bunyamin <bunyamin250391@gmail.com>
2024-02-27 21:42:55 +0700

    switch to sqlite

--- db/db.go
+++ db/db.go
@@ -2,28 +2,15 @@ package db
 
 import (
 	"database/sql"
-	"fmt"
 
-	_ "github.com/lib/pq"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 var DB *sql.DB
 
-const (
-	host     = "localhost"
-	port     = 5433
-	user     = "go-rest-api"
-	password = "qwerty123"
-	dbname   = "go-rest-api"
-)
-
 func InitDB() {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
 	var err error
-	DB, err = sql.Open("postgres", psqlInfo)
+	DB, err = sql.Open("sqlite3", "api.db")
 
 	if err != nil {
 		panic("Could not connect to database.")
@@ -32,27 +19,25 @@ func InitDB() {
 	DB.SetMaxOpenConns(10)
 	DB.SetMaxIdleConns(5)
 
-	defer DB.Close()
 	createTables()
 }
 
 func createTables() {
 	createEventsTable := `
 	CREATE TABLE IF NOT EXISTS events (
-		id SERIAL PRIMARY KEY,
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		name TEXT NOT NULL,
 		description TEXT NOT NULL,
 		location TEXT NOT NULL,
-		dateTime TIMESTAMP NOT NULL,
-		user_id INTEGER,
-		created_at TIMESTAMP not null,
-		updated_at TIMESTAMP not null
+		dateTime DATETIME NOT NULL,
+		user_id INTEGER
 	)
 	`
 
 	_, err := DB.Exec(createEventsTable)
 
 	if err != nil {
-		panic(err)
+		panic("Could not create events table.")
 	}
+
 }
