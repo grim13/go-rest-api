d3457aab88a6244b67fd1a1c35df29141dc46046
muhammad bunyamin <bunyamin250391@gmail.com>
2024-02-29 14:56:39 +0700

    add middleware feature

--- routes/events.go
+++ routes/events.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/grim13/go-rest-api/models"
-	"github.com/grim13/go-rest-api/utils"
 )
 
 func getEvent(context *gin.Context) {
@@ -35,19 +34,9 @@ func getEvents(context *gin.Context) {
 }
 
 func createEvent(context *gin.Context) {
-	token := context.Request.Header.Get("Authorization")
-	if token == "" {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
-		return
-	}
-	userId, err := utils.VerifyToken(token)
-	if err != nil {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
-		return
-	}
 
 	var event models.Event
-	err = context.ShouldBindJSON(&event)
+	err := context.ShouldBindJSON(&event)
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{
 			"message": "Could not parse request data.",
@@ -55,6 +44,7 @@ func createEvent(context *gin.Context) {
 		return
 	}
 
+	userId := context.GetInt64("userId")
 	event.UserID = userId
 	err = event.Save()
 	if err != nil {
--- routes/routes.go
+++ routes/routes.go
@@ -1,13 +1,19 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/grim13/go-rest-api/middlewares"
+)
 
 func RegisterRoutes(server *gin.Engine) {
 	server.GET("/events", getEvents) //GET, POST, PUT, PATCH, DELETE
 	server.GET("/events/:id", getEvent)
-	server.POST("/events", createEvent)
-	server.PUT("/events/:id", updateEvent)
-	server.DELETE("/events/:id", deleteEvent)
+
+	autenticate := server.Group("/")
+	autenticate.Use(middlewares.Authenticate)
+	autenticate.POST("/events", createEvent)
+	autenticate.PUT("/events/:id", updateEvent)
+	autenticate.DELETE("/events/:id", deleteEvent)
 
 	server.POST("/signup", singup)
 	server.POST("/login", login)
